56b08c46e18880078b2f276283e24c861e3d6cba
Chesvin1 <chesvin1@hotmail.com>
2020-12-05 01:38:18 -0300

    Update dev-app-update

--- build.js
+++ build.js
@@ -18,10 +18,10 @@ function getCurrentPlatform(){
 builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
-        appId: 'helioslauncher',
-        productName: 'Helios Launcher',
+        appId: 'farfanialauncher',
+        productName: 'Farfania Launcher',
         artifactName: '${productName}-setup-${version}.${ext}',
-        copyright: 'Copyright © 2018-2020 Daniel Scalzi',
+        copyright: 'Copyright © 2020-2020 Chesvin1',
         directories: {
             buildResources: 'build',
             output: 'dist'
