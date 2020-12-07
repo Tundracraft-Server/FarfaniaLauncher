fd9856dec10d482bbbc86381d0d0af4526afdbec
Chesvin1 <chesvin1@hotmail.com>
2020-12-07 15:36:10 -0300

    Increase Timeout time of the distribution index

--- app/assets/js/mojang.js
+++ app/assets/js/mojang.js
@@ -85,7 +85,7 @@ exports.status = function(){
         request.get('https://status.mojang.com/check',
             {
                 json: true,
-                timeout: 2500
+                timeout: 10000
             },
             function(error, response, body){
 
