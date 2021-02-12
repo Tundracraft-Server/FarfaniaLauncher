b6782f10867570fd1b510168f4d562a5928db3e5
Tundracraft-Server <coronacraft320@gmail.com>
2021-02-12 13:36:30 -0500

    Changed bar colored ram

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -968,9 +968,9 @@ settingsMinRAMRange.onchange = (e) => {
     const max = (os.totalmem()-1000000000)/1000000000
 
     // Change range bar color based on the selected value.
-    if(sMinV >= max/2){
+    if(sMinV >= max/1.5){
         bar.style.background = '#e86060'
-    } else if(sMinV >= max/4) {
+    } else if(sMinV >= max/2) {
         bar.style.background = '#e8e18b'
     } else {
         bar.style.background = null
@@ -1000,9 +1000,9 @@ settingsMaxRAMRange.onchange = (e) => {
     const max = (os.totalmem()-1000000000)/1000000000
 
     // Change range bar color based on the selected value.
-    if(sMaxV >= max/2){
+    if(sMaxV >= max/1.5){
         bar.style.background = '#e86060'
-    } else if(sMaxV >= max/4) {
+    } else if(sMaxV >= max/2) {
         bar.style.background = '#e8e18b'
     } else {
         bar.style.background = null
